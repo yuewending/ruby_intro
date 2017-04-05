c1e4b1480f0d98553b9f4a71529666bb8c837421
Raghu Betina <raghu@firstdraft.com>
2017-04-05 12:48:36 -0500

    Add additional programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -6,4 +6,28 @@ class ProgramsController < ApplicationController
 
     render("programs/home.html.erb")
   end
+
+  def first_program
+    # Your code goes here.
+
+    @your_output = "Replace this string with your output"
+
+    render("programs/first_program.html.erb")
+  end
+
+  def second_program
+    # Your code goes here.
+
+    @your_output = "Replace this string with your output"
+
+    render("programs/second_program.html.erb")
+  end
+
+  def third_program
+    # Your code goes here.
+
+    @your_output = "Replace this string with your output"
+
+    render("programs/third_program.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "programs", :action => "home" })
+
+  get("/first", { :controller => "programs", :action => "first_program" })
+
+  get("/second", { :controller => "programs", :action => "second_program" })
+
+  get("/third", { :controller => "programs", :action => "third_program" })
 end
